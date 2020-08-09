f4407822df093f684830eb2d9f363e4e476461dc
Brendalikukuvali <brendalikukuvali@gmail.com>
2020-08-09 14:32:45 +0300

    commit message

--- app/src/main/java/com/example/hello/CoursesActivity.kt
+++ app/src/main/java/com/example/hello/CoursesActivity.kt
@@ -1,33 +1,54 @@
-gipackage com.example.hello
+package com.example.hello
 
-//import androidx.appcompat.app.AppCompatActivity
-//import android.os.Bundle
-
-//class CoursesActivity : AppCompatActivity() {
-    //override fun onCreate(savedInstanceState: Bundle?) {
-        //super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_courses)
-    //}
-//}
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_registration.*
-class MainActivity : AppCompatActivity() {
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_courses.*
+
+
+class CoursesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        btnbutton.setOnClickListener() {
-            var Name = etName.text.toString()
-            var password = etPassword.text
-            Toast.makeText(baseContext, Name, Toast.LENGTH_LONG).show()
+        setContentView(R.layout.activity_courses)
+    }
+        data class Coursesactivity(val courses_id:Int, val courses_name:String, val course_code:Int, val instructor:String, val Description:String)
+        class coursesactivity : AppCompatActivity() {
+            override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                setContentView(R.layout.activity_courses)
+                rvCourses.layoutManager = LinearLayoutManager(baseContext)
+                CoursesActivityRecyclerViewAdapter(
+                    coursesActivity = listOf(
+                        Coursesactivity(0, "mercy", 11, "Brenda", "Kotlin project1"),
+                        Coursesactivity(1, "mercy", 12, "Anne", "Java project2"),
+                        Coursesactivity(2, "mercy", 13, "Angel", "Html project3"),
+                        Coursesactivity(4, "mercy", 14, "Bella", "Css project4"),
+                        Coursesactivity(5, "mercy", 15, "Lisa", "Design project5"),
+                        Coursesactivity(6, "mercy", 16, "Felisters", "Electonics project6"),
+                        Coursesactivity(7, "mercy", 17, "Gabriella", "Python project6"),
+                        Coursesactivity(8, "mercy", 18, "Nancy", "Machine project7"),
+                        Coursesactivity(9, "mercy", 19, "Shantel", "Kotli2 project9"),
+                        Coursesactivity(10, "mercy", 20, "Valencia", "Python2 project9")
+
+                    )
+                );
+
+
+                val rvCoursesActivity = null
+                val coursesAdapter = null
+                val value: Nothing? = rvCoursesActivity
+
+
+
+            }
+
+            private fun CoursesActivityRecyclerViewAdapter(
+                listOf: List<Any>,
+                coursesActivity: List<Coursesactivity>
+            ) = Unit
+
         }
     }
-
-
-
 
 
 
---
+++ app/src/main/res/layout/CoursesRecyclerViewAdaptet.kt
@@ -0,0 +1,25 @@
+package layout
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hello.R
+import kotlinx.android.synthetic.main.row_courses_item.view.*
+
+
+class CoursesRecyclerViewAdapter(val CoursesList: List<String>) :
+    RecyclerView.Adapter<CoursesRecyclerViewAdapter.NamesViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NamesViewHolder {
+        var itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.row_courses_item, parent, false)
+        return NamesViewHolder(itemView)
+    }
+    override fun getItemCount(): Int {
+        return CoursesList.size
+    }
+    override fun onBindViewHolder(holder: NamesViewHolder, position: Int) {
+        holder.rowView.tvCourses.text = CoursesList[position]
+    }
+    class NamesViewHolder(val rowView: View) : RecyclerView.ViewHolder(rowView)
+}
